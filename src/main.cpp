210bdb797b230089ef0aef396040afa5fab0fef2
IgorChukarin <Zigor2356@mail.ru>
2024-11-10 13:11:02 +0100

    task class added

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,18 @@
 int myFunction(int, int);
 
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(2000);
+  digitalWrite(13, LOW);
+  delay(2000);
 }
 
 // put function definitions here:
