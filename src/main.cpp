2b5aa8a41ee8b3e357e2b4466667ab14ee8ff5f4
keni <kenitafili@gmail.com>
2024-11-27 12:36:21 +0100

    add scheduler for real ;) I hope

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#include "Arduino.h"
 #include "ContainerManagementTask.h"
 #include "Scheduler.h"
+#include "Arduino.h"
 
+
+Scheduler sched;
 Scheduler sched;
 
 void setup() {
@@ -10,8 +12,13 @@ void setup() {
   Task* t0 = new ContainerManagementTask();
   t0->init(100); 
   sched.addTask(t0);
+  sched.init(100);
+  Task* t0 = new ContainerManagementTask();
+  t0->init(100); 
+  sched.addTask(t0);
 }
 
 void loop() {
+  sched.schedule();
   sched.schedule();
 }
